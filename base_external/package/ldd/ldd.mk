fe21dffab7fe8e10162e0c5b3cf6b0ddc9cb437b
daniel <hangon1234@gmail.com>
2024-11-10 13:45:31 +0900

    add new package for assignment 7

---
+++ base_external/package/ldd/ldd.mk
@@ -0,0 +1,9 @@
+##############################################################
+#
+# Assignment 7 - Building and installing misc-modules
+#                and scull components
+#
+##############################################################
+
+LDD_VERSION = '09e35eb64eedba31cee638b0d6e230d348bcfe7d'
+
