7b39dc74ff1662f9941d9602155f9b0a2b90b909
daniel <hangon1234@gmail.com>
2024-11-10 14:29:33 +0900

    add new package to the image - ldd

--- base_external/package/ldd/ldd.mk
+++ base_external/package/ldd/ldd.mk
@@ -6,4 +6,14 @@
 ##############################################################
 
 LDD_VERSION = '09e35eb64eedba31cee638b0d6e230d348bcfe7d'
+LDD_SITE = 'git@github.com:cu-ecen-aeld/assignment-7-hangon1234.git'
+LDD_SITE_METHOD = git
+LDD_LICENSE = GPL-2.0
+
+LDD_MODULE_SUBDIRS = misc-modules
+LDD_MODULES_SUBDIRS += scull
+LDD_MODULE_MAKE_OPTS = KVERSION=$(LINUX_VERSION_PROBED)
+
+$(eval $(kernel-module))
+$(eval $(generic-package))
 
